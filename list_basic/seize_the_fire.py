9f2de1dc7a2e575e527747f9521cc140464f6aa8
iliyna <114064714+iliyna@users.noreply.github.com>
2022-10-04 11:00:58 +0200

    pass
    pass

--- list_basic/seize_the_fire.py
+++ list_basic/seize_the_fire.py
@@ -1,4 +1,54 @@
 the_level_of_fire = input().split('#')
-a = the_level_of_fire
+amount_of_water = int(input())
+print(f'Cells:')
+x = the_level_of_fire
+x.reverse()
 
-print(a)
+a = (x[- 1]).split()
+
+b = len(x)
+type_of_fire = a[0]
+ranges = int(a[2])
+extinguished_fires = []
+total_fire = 0
+effort = 0
+
+for i in range(b):
+
+    if amount_of_water <= 0:
+        break
+
+    if type_of_fire == 'High':
+        if 81 <= ranges <= 125 and amount_of_water >= ranges:
+            amount_of_water -= ranges
+            total_fire += ranges
+            effort += ranges * 0.25
+            extinguished_fires.append(ranges)
+            print(f' - {ranges}')
+
+    elif type_of_fire == 'Medium':
+        if 51 <= ranges <= 80 and amount_of_water >= ranges:
+            amount_of_water -= ranges
+            total_fire += ranges
+            effort += ranges * 0.25
+            extinguished_fires.append(ranges)
+            print(f' - {ranges}')
+
+    elif type_of_fire == 'Low' and amount_of_water >= ranges:
+        if 1 <= ranges <= 50:
+            amount_of_water -= ranges
+            total_fire += ranges
+            effort += ranges * 0.25
+            extinguished_fires.append(ranges)
+            print(f' - {ranges}')
+
+    x.pop()
+    if i == b - 1:
+        break
+    a = (x[- 1]).split()
+    type_of_fire = a[0]
+    ranges = int(a[2])
+
+
+print(f'Effort: {effort:.2f}')
+print(f'Total Fire: {total_fire}')
